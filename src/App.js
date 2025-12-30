1b148993329628a18891d76f7332fd5286302e94
Szymon Jaworski <jaworski10293@gmail.com>
2025-12-30 16:32:05 +0100

    added Clock

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Container from "./Container";
 import Header from "./Header";
 import Form from "./Form";
+import Clock from "./Clock";
 
 function App() {
   return (
     <Container>
-      <Header title="Przelicznik walut" />
-      <Form />
-      <p className="form__info">
-        według średniego kursu NBP z dn. 14.11.2025
-      </p>
+      <main>
+        <Clock /> { }
+        <Header title="Przelicznik walut" />
+        <Form />
+        <p className="form__info">
+          według średniego kursu NBP z dn. 14.11.2025
+        </p>
+      </main>
     </Container>
   );
 }
---
+++ src/Clock/index.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+const Clock = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div className="clock">
+            Dzisiaj jest
+            {" "}
+            {date.toLocaleString(undefined, {
+                weekday: "long",
+                day: "numeric",
+                month: "long",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            })}
+        </div>
+    );
+};
+
+export default Clock;
